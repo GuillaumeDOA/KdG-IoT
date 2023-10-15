d018dfb93b19c48658d51fa58e42ea287c6f00d7
Guillaume <guillaume2000@hotmail.be>
2023-10-16 00:01:34 +0200

    Project Advances

--- Build 1/Oefeningen/Mini Huis Project/src/main.cpp
+++ Build 1/Oefeningen/Mini Huis Project/src/main.cpp
@@ -1,14 +1,127 @@
 #include <Arduino.h>
+
+// Include necessary libraries
 #include <Servo.h>
+#include "DHT.h"
+#include <Adafruit_Sensor.h>
+#include <U8g2lib.h>
+#include <Wire.h>
+#include <ezButton.h>
 
-#define PIRSensor 6
+// Define pins
+#define SERVO 10
+#define RGB_RED 3
+#define RGB_GREEN 5
+#define RGB_BLUE 6
+#define BUTTON_WHITE A0
+#define BUTTON_RED A1
+#define BUTTON_YELLOW A2
+#define DHTPIN 2
+#define MOTIONSENSOR 9
 
-Servo deur;
+// Initialise OLED display
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C Display(U8G2_R0, /* clock=*/12, /* data=*/11, /* reset=*/U8X8_PIN_NONE); // Software I2C
 
-void setup(){
-  deur.attach(5);
+// Initialise Temp sensor
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+// Initialise Servo
+Servo Door;
+
+// Initialise ezButtons
+ezButton redButton(BUTTON_RED);
+ezButton whiteButton(BUTTON_WHITE);
+ezButton yellowButton(BUTTON_YELLOW);
+
+// Initialise variables
+float Temp;
+float Humidity;
+float lastTemp;
+float lastHumidity;
+int motionState = LOW; // by default, no motion detected
+int motionVal = 0;     // variable to store the sensor status (value)
+
+void setup()
+{
+  Door.attach(SERVO); // attach servo
+
+  Display.begin(); // Start OLED display
+  Display.setFont(u8g2_font_luBIS08_tf);
+  Display.clearBuffer();
+
+  pinMode(LED_BUILTIN, OUTPUT); // Set built in LED as OUTPUT
+
+  pinMode(MOTIONSENSOR, INPUT);
+
+  pinMode(DHTPIN, INPUT);
+  dht.begin(); // Start Temp and Humid sensor
+
+  redButton.setDebounceTime(100);
+  yellowButton.setDebounceTime(100);
+  whiteButton.setDebounceTime(50);
 }
 
-void loop(){
-  
+void readTemp()
+{
+  Temp = dht.readTemperature();
+  Humidity = dht.readHumidity();
+
+  if (lastTemp == Temp || lastHumidity == Humidity)
+    return;
+  else
+  {
+    lastTemp = Temp;
+    lastHumidity = Humidity;
+
+    Display.clearBuffer();
+    Display.setCursor(0, 10);
+    Display.print("Temp: ");
+    Display.print(Temp);
+    Display.setCursor(0, 30);
+    Display.print("Humidity: ");
+    Display.print(Humidity);
+    Display.sendBuffer();
+  }
+}
+
+void readMotion()
+{
+  motionVal = digitalRead(MOTIONSENSOR); // read sensor motionValue
+  if (motionVal == HIGH)
+  { // check if the sensor is HIGH
+    if (motionState == LOW)
+    {
+      motionState = HIGH; // update variable state to HIGH
+      Door.write(180);
+    }
+  }
+  else
+  {
+    if (motionState == HIGH)
+    {
+      motionState = LOW; // update variable state to LOW
+      Door.write(0);
+    }
+  }
+}
+
+void buttonTest()
+{
+  if (whiteButton.isPressed())
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+}
+
+void loop()
+{
+  // Display.clearBuffer();
+  redButton.loop();
+  whiteButton.loop();
+  yellowButton.loop();
+
+  readTemp();
+  readMotion();
+  buttonTest();
+
+  // Display.sendBuffer();
 }
