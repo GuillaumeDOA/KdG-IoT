6080bae4addb0dcb5da6b427e170affc682f6d16
Guillaume de Oliveira <guillaume2000@hotmail.be>
2023-10-18 18:19:49 +0200

    BUILD: Mini huis v1.0

--- Build 1/Oefeningen/Mini Huis Project/src/main.cpp
+++ Build 1/Oefeningen/Mini Huis Project/src/main.cpp
@@ -17,13 +17,11 @@
 #define BUTTON_RED A1
 #define BUTTON_YELLOW A2
 #define DHTPIN 2
-#define MOTIONSENSOR 9
+#define MOTIONSENSOR 13
 #define OLED_CLOCK 12 // YELLOW WIRE
 #define OLED_DATA 11  // WHITE WIRE
-#define DOORBELL 4
-#define ROTORY_CLOCK 7
-#define ROTORY_DATA 8
-#define ROTORY_SWITCH A3
+#define BUZZER 4
+#define DOORBELL_BUTTON A3
 
 // Initialise OLED display
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C Display(U8G2_R0, OLED_CLOCK, OLED_DATA, /* reset=*/U8X8_PIN_NONE); // Software I2C
@@ -39,23 +37,16 @@ Servo Door;
 ezButton redButton(BUTTON_RED);
 ezButton whiteButton(BUTTON_WHITE);
 ezButton yellowButton(BUTTON_YELLOW);
-ezButton rotarySwitch(ROTORY_SWITCH);
+ezButton doorBell(DOORBELL_BUTTON);
 
 // Initialise variables
 float Temp;
 float Humidity;
-float lastTemp;
-float lastHumidity;
 long startTimeTemp;
-long startTimeDoor;
-long lastRotaryPress = 0;
 int DHTDelay = 2000;
 int motionState = LOW;   // by default, no motion detected
 int motionVal = 0;       // variable to store the sensor status (value)
 bool RGB_Status = false; // TRue = ON; False = OFF
-int currentStateCLK;
-int lastStateCLK;
-int frequency = 500;
 
 void setup()
 {
@@ -64,13 +55,11 @@ void setup()
   Display.begin(); // Start OLED display
   Display.setFont(u8g2_font_luBIS08_tf);
 
-  pinMode(ROTORY_CLOCK, INPUT); // Setup rotary encoder as INPUT
-  pinMode(ROTORY_DATA, INPUT);
-  lastStateCLK = digitalRead(ROTORY_CLOCK);
-
-  pinMode(LED_BUILTIN, OUTPUT); // Set built in LED as OUTPUT
-
   pinMode(MOTIONSENSOR, INPUT);
+
+  pinMode(RGB_BLUE, OUTPUT);
+  pinMode(RGB_RED, OUTPUT);
+  pinMode(RGB_GREEN, OUTPUT);
 
   pinMode(DHTPIN, INPUT);
   dht.begin(); // Start Temp and Humid sensor
@@ -78,12 +67,13 @@ void setup()
   redButton.setDebounceTime(100);
   yellowButton.setDebounceTime(100);
   whiteButton.setDebounceTime(100);
-  rotarySwitch.setDebounceTime(100);
+  doorBell.setDebounceTime(100);
 
   analogWrite(RGB_RED, 255); // Turn off led on startup
   analogWrite(RGB_GREEN, 255);
   analogWrite(RGB_BLUE, 255);
-  startTimeTemp = startTimeDoor = millis();
+
+  startTimeTemp = millis();
 }
 
 void ReadTemp()
@@ -104,44 +94,13 @@ void ReadTemp()
       Display.setCursor(0, 30);
       Display.print("Humidity: ");
       Display.print(Humidity);
-      // Display.sendBuffer();
     } while (Display.nextPage());
     startTimeTemp += DHTDelay;
   }
 }
-// {
-//   currentMillis = millis();
-//   Temp = dht.readTemperature();
-//   Humidity = dht.readHumidity();
-
-//   if (currentMillis - startMillis >= DHTDelay)
-//   {
-//     startMillis += DHTDelay;
-//     Display.clearBuffer();
-
-//     if (isnan(Humidity) || isnan(Temp))
-//     {
-//       Display.setCursor(0, 10);
-//       Display.print("Failed to read from DHT");
-//       Display.sendBuffer();
-//       return;
-//     }
-
-//     Display.setCursor(0, 10);
-//     Display.print("Temp: ");
-//     Display.print(Temp);
-//     Display.setCursor(0, 30);
-//     Display.print("Humidity: ");
-//     Display.print(Humidity);
-//     Display.sendBuffer();
-//   }
-// }
 
 void ReadMotion()
 {
-  if (millis() - startTimeDoor < 3000)
-    return;
-
   motionVal = digitalRead(MOTIONSENSOR); // read sensor motionValue
   if (motionVal == HIGH)
   { // check if the sensor is HIGH
@@ -196,32 +155,11 @@ void ChangeRGB()
   }
 }
 
-void ReadDoorbell()
+void DoorBell()
 {
-  currentStateCLK = digitalRead(ROTORY_CLOCK);
-
-  if (currentStateCLK != lastStateCLK && currentStateCLK == 1)
+  if (doorBell.isPressed())
   {
-    if (digitalRead(ROTORY_DATA) == currentStateCLK)
-    {
-      if (frequency != 0)
-        frequency -= 50;
-    }
-    else
-    {
-      if (frequency != 20000)
-        frequency += 50;
-    }
-    Display.setCursor(0, 50);
-    Display.print("Frequency");
-    Display.print(frequency);
-  }
-
-  lastStateCLK = currentStateCLK;
-
-  if (rotarySwitch.isPressed())
-  {
-    tone(DOORBELL, frequency, 500);
+    tone(BUZZER, 329.6, 400); // tone(PIN#, FREQUENCY)
   }
 }
 
@@ -230,9 +168,9 @@ void loop()
   redButton.loop();
   whiteButton.loop();
   yellowButton.loop();
-  rotarySwitch.loop();
+  doorBell.loop();
 
-  ReadDoorbell();
+  DoorBell();
   ReadTemp();
   ReadMotion();
   ChangeRGB();
