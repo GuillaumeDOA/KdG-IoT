bb5c52e2a93aa6857c2d16f9d9dade005136a0e5
Guillaume de Oliveira <guillaume2000@hotmail.be>
2023-12-18 14:07:31 +0100

    SIMULATE: Code opdracht 22/12

--- Simulate 1/Engredi Project/src/main.cpp
+++ Simulate 1/Engredi Project/src/main.cpp
@@ -1,16 +1,103 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <Wire.h>
-
 #include "Adafruit_PM25AQI.h"
+#include <WiFiS3.h>
+
+#define powerLED 3
+#define wifiLED 4
+#define dataLED 5
 
 Adafruit_PM25AQI aqi = Adafruit_PM25AQI();
 
-void setup() {
-  // Wait for serial monitor to open
-  Serial.begin(115200);
-  while (!Serial) delay(10);
 
+long startTime;
+long delayTime = 2000;
+
+char ssid[] = "Home de Oliveira - Haesaert";
+char pass[] = "0475212120";
+int status = WL_IDLE_STATUS;
+
+void printMacAddress(byte mac[]) {
+  for (int i = 0; i < 6; i++) {
+    if (i > 0) {
+      Serial.print(":");
+    }
+    if (mac[i] < 16) {
+      Serial.print("0");
+    }
+    Serial.print(mac[i], HEX);
+  }
+  Serial.println();
+}
+
+void printWifiData() {
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  
+  Serial.println(ip);
+
+  // print your MAC address:
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  printMacAddress(mac);
+}
+
+void printCurrentNet() {
+  // print the SSID of the network you're attached to:
+  Serial.println("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the MAC address of the router you're attached to:
+  byte bssid[6];
+  WiFi.BSSID(bssid);
+  Serial.print("BSSID: ");
+  printMacAddress(bssid);
+
+}
+
+void WifiSetup()
+{
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE)
+  {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true)
+    {
+      Serial.println("Problem with Wifi module");
+      delay(5000);
+    }
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION)
+  {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED)
+  {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  // you're connected now, so print out the data:
+  digitalWrite(wifiLED, HIGH);
+  Serial.print("You're connected to the network");
+  printCurrentNet();
+  printWifiData();
+}
+
+void SensorSetup(){
   Serial.println("Adafruit PMSA003I Air Quality Sensor");
 
   // Wait one second for sensor to boot up!
@@ -22,14 +109,19 @@ void setup() {
   }
 
   Serial.println("PM25 found!");
+
 }
 
 void readAQI(){
+  
+  if(millis() - startTime < delayTime)
+    return;   // Read Sensor every 2 seconds
+
+  digitalWrite(dataLED, HIGH);
   PM25_AQI_Data data;
   
   if (! aqi.read(&data)) {
-    Serial.println("Could not read from AQI");
-    delay(500);  // try again in a bit!
+    Serial.println("Could not read from AQI"); // Try again
     return;
   }
   Serial.println("AQI reading success");
@@ -55,8 +147,27 @@ void readAQI(){
   Serial.print(F("Particles > 10 um / 0.1L air:")); Serial.println(data.particles_100um);
   Serial.println(F("---------------------------------------"));
   
+  startTime = millis();
+  digitalWrite(dataLED, LOW);
+}
 
-  delay(1000);
+void setup() {
+  pinMode(powerLED, OUTPUT);
+  pinMode(wifiLED, OUTPUT);
+  pinMode(dataLED, OUTPUT);
+
+  // Powerled high as soon as program starts
+  digitalWrite(powerLED, HIGH);
+
+
+  // Wait for serial monitor to open
+  Serial.begin(115200);
+  while (!Serial) delay(10);
+
+  SensorSetup();
+  WifiSetup();
+
+  startTime = millis();
 }
 
 void loop() {
