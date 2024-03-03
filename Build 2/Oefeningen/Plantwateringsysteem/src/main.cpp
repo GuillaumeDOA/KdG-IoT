13407720ffa9fe6ac27bcb8a6657c874e5f2162a
Guillaume de Oliveira <guillaume2000@hotmail.be>
2024-03-03 13:52:18 +0100

    BUILD: Plantwatering Project started

--- Build 2/Oefeningen/Plantwateringsysteem/src/main.cpp
+++ Build 2/Oefeningen/Plantwateringsysteem/src/main.cpp
@@ -1,9 +1,105 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <time.h>
+#include <Firebase_ESP_Client.h>
+#include <addons/RTDBHelper.h>
+#include <addons/TokenHelper.h>
+#include "secrets.h"
+#include "config.h"
 
-void setup(){
+#define RESISTIVE_SENSOR A0
+#define CAPACITIVE_SENSOR A1
 
+// Setup ntp time server
+const char *ntpServer = "pool.ntp.org";
+
+// Firebase config variables
+FirebaseData fbdo;
+FirebaseConfig config;
+FirebaseAuth auth;
+FirebaseJson json;
+
+// Firebase paths
+String parentPath;
+String databasePath = "/readings";
+String timePath = "/timestamp";
+String resistivePath = "/resistive";
+String capacitivePath = "/capacitive";
+String statusPath = "/status";
+
+int timestamp;
+unsigned long startTime;
+unsigned long timerDelay = 5000;
+
+// Setup WiFi
+void setupWifi()
+{
+    WiFi.begin(SSID, PASSWORD);
+    Serial.print("Connecting to WiFi ..");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print('.');
+        delay(1000);
+    }
+    Serial.println("Wifi connected!");
 }
 
-void loop(){
+// Gets current time in epoch format
+unsigned long getTime()
+{
+    time_t now;
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo))
+    {
+        // Serial.println("Failed to obtain time");
+        return (0);
+    }
+    time(&now);
+    return now;
+}
 
+void setup()
+{
+    Serial.begin(115200);
+
+    // Setup Sensors
+    pinMode(RESISTIVE_SENSOR, INPUT);
+    pinMode(CAPACITIVE_SENSOR, INPUT);
+
+    setupWifi();
+
+    // Setup time server
+    configTime(0, 0, ntpServer);
+
+    // Setup Firebase RTDB
+    config.api_key = API_KEY;
+    config.database_url = DATABASE_URL;
+    Firebase.reconnectWiFi(true);
+    fbdo.setResponseSize(4096);
+    config.token_status_callback = tokenStatusCallback;
+    config.max_token_generation_retry = 5;
+
+    Firebase.begin(&config, &auth);
+}
+
+void loop()
+{
+    if (Firebase.ready() && (millis() - startTime > timerDelay))
+    {   
+        // Get current timestamp
+        timestamp = getTime();
+
+        parentPath = databasePath + "/" + String(timestamp);
+
+        // Prepare json
+        json.set(resistivePath, "TESTING RESISTIVE");
+        json.set(capacitivePath, "TESTING CAPACITIVE");
+        json.set(statusPath, "TESTING STATUS");
+        json.set(timePath, timestamp);
+
+        // Send json and capture error
+        Serial.printf("Set json %s\n", Firebase.RTDB.setJSON(&fbdo, parentPath.c_str(), &json) ? "ok" : fbdo.errorReason().c_str());
+
+        startTime = millis();
+    }
 }
---
+++ Build 2/Oefeningen/Plantwateringsysteem/src/secrets.h
@@ -0,0 +1,8 @@
+// Wifi Credentials
+#define SSID "Home de Oliveira - Haesaert"
+#define PASSWORD "0475212120"
+
+// Firebasae Credentials
+#define API_KEY  "AIzaSyBQ5hrN7VhE4vtUQcQYAoqnOiMIstBpHi8"
+#define DATABASE_URL "https://plantwatering-system-default-rtdb.europe-west1.firebasedatabase.app"
+
