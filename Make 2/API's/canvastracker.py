da2c26dd38e104ef241b2e1051d90b76497c9d14
Guillaume de Oliveira <guillaume2000@hotmail.be>
2024-04-20 14:49:54 +0200

    MAKE: Canvastracker finished

--- Make 2/API's/canvastracker.py
+++ Make 2/API's/canvastracker.py
@@ -1,38 +1,56 @@
 import requests
 
-
 token = "12400~M8P1vXZT5hkCNG6LsGY0blnvOUngy4oNDnJFlLwniv0ady6YnAv8ZDsRlyLD4eNx"
 base = "https://canvas.kdg.be"
 
+def checkError(response):
+    if response.status_code != 200:
+        print(f"ERROR: API request '{response.url}' failed")
+        exit(1)
+
 def getCourses():
     result = requests.get(f"{base}/api/v1/courses?access_token={token}&enrollment_state=active")
-    return result.json()
+    checkError(result)
+    result = result.json()
+    res = {}
+    for i in result:
+        if any(substring in i["name"] for substring in ["Make", "Build", "Improve"]):
+            res[i["name"]] = i["id"]
+    return res
+
 
 def getUserID():
     result = requests.get(f"{base}/api/v1/users/self?access_token={token}")
-    return result.json()['id']
+    checkError(result)
+    return result.json()["id"]
 
 def getAssignments(courseID):
-    result = requests.get(f"{base}/api/v1/courses/{courseID}/assignments?access_token={token}")
+    result = requests.get(f"{base}/api/v1/courses/{courseID}/assignments?access_token={token}&bucket=past")
+    checkError(result)
     return result.json()
 
 def getSubmissions(courseID, assignmentID):
     result = requests.get(f"{base}/api/v1/courses/{courseID}/assignments/{assignmentID}/submissions/{userID}?access_token={token}&include=full_rubric_assessment")
+    checkError(result)
     return result.json()
 
 userID = getUserID()
 courses = getCourses()
 
-for course in courses:
-    if any(substring in course['name'] for substring in ['Make', 'Build', 'Improve']):
-        print(f"COURSE:  {course['name'].upper()} ({course['id']})")
-        print("----------------------------------------------")
-        assignments = getAssignments(course['id'])
-        for assignment in assignments:
-            submission = getSubmissions(course['id'], assignment['id'])
+for courseName in courses.keys():
+    print(f"{courseName.upper()} ({courses[courseName]})")
+    print("----------------------------------------------")
+    assignments = getAssignments(courses[courseName])
+    for assignment in assignments:
+        if assignment['graded_submissions_exist']:
+            submission = getSubmissions(courses[courseName], assignment['id'])
             grades = []
-            # for grade in submission['full_rubric_assessment']['data']:
-            #     grades.append(grade['description'])
-            # if 'Niet Goed' in grades or 'Bijna Goed' in grades:
-            print(f"{assignment['name']}: {submission['full_rubric_assessment']}")
-        print()
+            if "full_rubric_assessment" not in submission.keys():
+                continue
+            for grade in submission['full_rubric_assessment']["data"]:
+                grades.append(grade['description'])
+            if any(substring in grades for substring in ["Bijna goed","Nog niet goed", "Niet ingediend"]):
+                print(f"{assignment['name']} ({assignment["id"]}) --> NIET OK!")
+            # else:
+            #     print(f"{assignment['name']} ({assignment["id"]}) --> OK!")
+    print()
